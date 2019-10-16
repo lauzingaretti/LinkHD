ce4d98f597b953149df1636314ab7d2705540ba1
lauzingaretti <m.lau.zingaretti@gmail.com>
2019-10-16 16:38:18 +0200

    Add files via upload

--- R/ComputeDistance.R
+++ R/ComputeDistance.R
@@ -1,96 +1,59 @@
 #' @import vegan
-ComputeDistance<-function(X,Distance,Z=diag(1,nrow=dim(X))){
-
-  #distance funtions to use
-  #1. ScalarProduct
-  #2. euclidean
-  #3. manhatan
-  #4. canberra
-  #5. pearson
-  #6. pearsonabs
-  #7. spearman
-  #8. spearmanabs
-  #9. mahalanobis
-  #binary Data
-  #10. jaccard
-  #11. sm
-  #12. sokal&Sneath
-  #13. RogeryTanimoto
-  #14. Dice
-  #15. Hamman
-  #16. Ochiai
-  #17. Sokal&Sneath
-  #18. Phi de Pearson
-  #19. Gower&Leendre
-  #compositional data
-
-  if (tolower(Distance)=="scalarproduct") Distance <- "ScalarProduct" else #(1)
-    if (tolower(Distance)=="euclidean") Distance <- "euclidean" else #(2)
-     if (tolower(Distance)=="bray") Distance <- "bray" else #(2bis)
-      if (tolower(Distance)=="manhattan") Distance <- "manhattan" else #(3)
-        if (tolower(Distance)=="canberra") Distance <- "canberra" else #(4)
-          if (tolower(Distance)=="pearson") Distance <- "pearson" else #(5)
-            if (tolower(Distance)=="pearsonabs") Distance <- "pearsonabs" else #(6)
-              if (tolower(Distance)=="spearman") Distance <- "spearman" else #(7)
-                if (tolower(Distance)=="spearmanabs") Distance <- "spearmanabs" else #(8)
-                  if (tolower(Distance)=="mahalanobis") Distance <- "mahalanobis" else #(9)
-                    if (tolower(Distance)=="jaccard") Distance <- as.numeric(1)  else #(10)
-                      if (tolower(Distance)=="simple matching") Distance <- as.numeric(2)  else #(11)
-                        if (tolower(Distance)=="sokal") Distance <- as.numeric(3) else #(12)
-                          if (tolower(Distance)=="rogers&tanimoto") Distance <- as.numeric(4) else #(13)
-                            if (tolower(Distance)=="dice") Distance <- as.numeric(5) else #(14)
-                              if (tolower(Distance)=="Hamman") Distance <- as.numeric(6) else #(15)
-                                if (tolower(Distance)=="Ochiai") Distance <- as.numeric(7) else #(16)
-                                  if (tolower(Distance)=="sokal2") Distance <- as.numeric(8) else #(17)
-                                    if (tolower(Distance)=="Phi-Pearson") Distance <- as.numeric(9) else #(18)
-                                      if (tolower(Distance)=="Gower&Legendre") Distance <- as.numeric(10) else #(19)
-
-
-                                        Distance <- "ScalarProduct"
-
-
-                                      if(Distance=="ScalarProduct"){
-                                        if (compbin(X)=="Binary Data Imput"){
-                                          stop("Your Data are Binary and the Distance chosen is for continuous data ")
-
-                                        }
-                                        S <- (X)%*%t(X)
-                                      }
-                                      if(Distance=="mahalanobis"){
-                                        if (compbin(X)=="Binary Data Imput"){
-                                          stop("Your Data are Binary and the Distance chosen is for continuous data ")
-
-                                        }
-                                        S <- as.matrix(maha(X))
-                                      }
-                                      if(Distance=="euclidean" || Distance=="manhattan" ||Distance=="canberra"||Distance=="pearson" ||
-                                         Distance=="pearsonabs"|| Distance=="spearman"|| Distance=="spearmanabs"){
-                                        if (compbin(X)=="Binary Data Imput"){
-                                          stop("Your Data are Binary and the Distance chosen is for continuous data ")
-                                        }
-                                        S <- -1/2*(Z%*%as.matrix(distan(X, meth.dis = Distance, diag = TRUE, upper = TRUE))%*%t(Z))
-                                      }
-                                      if(Distance=="bray"){
-                                        if (compbin(X)=="Binary Data Imput"){
-                                        S <- -1/2*(Z%*%as.matrix(vegan::vegdist(X, method=Distance,binary=TRUE))%*%t(Z))
-                                          }else{
-                                        S <- -1/2*(Z%*%as.matrix(vegan::vegdist(X, method=Distance,binary=FALSE))%*%t(Z))
-
-                                        }
-                                      }
-
-
-
-                                      if(Distance==1|| Distance==2 ||Distance==3||Distance==4 ||
-                                         Distance==5|| Distance==6|| Distance==7|| Distance==8 ||
-                                         Distance==9 || Distance==10){
-                                        if(compbin(X)=="Binary Data Imput"){
-                                          S<- -1/2*(Z%*%as.matrix(dist.binary(X, method = as.numeric(Distance), diag = TRUE, upper = TRUE))%*%t(Z))
-                                        }
-                                        if(compbin(X)=="Non-Binary Data Imput"){
-                                          stop(paste("You chose the ", Distance,"for binary data and the table  is not binary-like"))
-                                        }
-                                      }
-
-                                      return(S)
+ComputeDistance <- function(X, Distance, Z = diag(1, nrow = dim(X))) {
+    
+    # 'distance funtions' to use: 1. ScalarProduct 2. euclidean 3. manhatan 4. canberra 5. pearson 6.
+    # pearsonabs 7. spearman 8.  spearmanabs 9. mahalanobis binary Data 10. jaccard 11. sm 12.  sokal&Sneath 13.
+    # RogeryTanimoto 14. Dice 15. Hamman 16. Ochiai 17. Sokal&Sneath 18. Phi de Pearson 19.  Gower&Leendre
+    # compositional data
+    Distance <- switch(tolower(Distance), scalarproduct = "ScalarProduct", euclidean = "euclidean", bray = "bray", 
+        manhattan = "manhattan", canberra = "canberra", pearson = "pearson", pearsonabs = "pearsonabs", spearman = "spearman", 
+        spearmanabs = "spearmanabs", mahalanobis = "mahalanobis", jaccard = as.numeric(1), simple_maching = as.numeric(2), 
+        sokal = as.numeric(3), roger_tanimoto = as.numeric(4), dice = as.numeric(5), hamman = as.numeric(6), 
+        ochiai = as.numeric(7), sokal2 = as.numeric(8), phi_pearson = as.numeric(9), gower_legendre = as.numeric(10))
+    
+    if (is.null(Distance)) {
+        Distance <- "ScalarProduct"
+    }
+    
+    if (Distance == "ScalarProduct") {
+        if (compbin(X) == "Binary Data Imput") {
+            stop("Your Data are Binary and the Distance chosen is for continuous data ")
+        }
+        S <- (X) %*% t(X)
+    }
+    if (Distance == "mahalanobis") {
+        if (compbin(X) == "Binary Data Imput") {
+            stop("Your Data are Binary and the Distance chosen is for continuous data ")
+        }
+        S <- as.matrix(maha(X))
+    }
+    if (Distance == "euclidean" || Distance == "manhattan" || Distance == "canberra" || Distance == "pearson" || 
+        Distance == "pearsonabs" || Distance == "spearman" || Distance == "spearmanabs") {
+        if (compbin(X) == "Binary Data Imput") {
+            stop("Your Data are Binary and the Distance chosen is for continuous data ")
+        }
+        S <- -1/2 * (Z %*% as.matrix(distan(X, meth.dis = tolower(Distance), diag = TRUE, upper = TRUE)) %*% 
+            t(Z))
+    }
+    if (Distance == "bray") {
+        if (compbin(X) == "Binary Data Imput") {
+            S <- -1/2 * (Z %*% as.matrix(vegan::vegdist(X, method = Distance, binary = TRUE)) %*% t(Z))
+        } else {
+            S <- -1/2 * (Z %*% as.matrix(vegan::vegdist(X, method = Distance, binary = FALSE)) %*% t(Z))
+            
+        }
+    }
+    
+    if (Distance == 1 || Distance == 2 || Distance == 3 || Distance == 4 || Distance == 5 || Distance == 6 || 
+        Distance == 7 || Distance == 8 || Distance == 9 || Distance == 10) {
+        if (compbin(X) == "Binary Data Imput") {
+            S <- -1/2 * (Z %*% as.matrix(dist.binary(X, method = as.numeric(Distance), diag = TRUE, upper = TRUE)) %*% 
+                t(Z))
+        }
+        if (compbin(X) == "Non-Binary Data Imput") {
+            stop(paste("You chose the ", Distance, "for binary data and the table  is not binary-like"))
+        }
+    }
+    
+    return(S)
 }
--- R/DistStatis-Class.R
+++ R/DistStatis-Class.R
@@ -20,17 +20,16 @@
 #'  \item RQO: Representation Quality of observations in compromise matrix.
 #'  \item TableProjections: Projection of each table on Compromise configuration
 #' }
-#' @section Accesors:
-#' \itemize{
-#'  \item RV: Vectorial Correlation Matrix between studies.
-#'  \item Inertia.RV: Inertia (\%) explained for all tables.
-#'  \item Euclid.Im: Euclidean Image of all studies.
-#'  \item Inertia.comp: Inertia (\%) explained for all dimensions of compromise matrix.
-#'  \item Compromise.Coords: Projection of all observations in compromise (Coords).
-#'  \item Compromise.Matrix: Compromise Matrix from statis methodology.
-#'  \item RQO: Representation Quality of observations in compromise matrix.
-#'  \item TableProjections: Projection of each table on Compromise configuration
-#' }
+#'
+#'  @slot RV: Vectorial Correlation Matrix between studies.
+#'  @slot  Inertia.RV: Inertia (\%) explained for all tables.
+#'  @slot  Euclid.Im: Euclidean Image of all studies.
+#'  @slot  Inertia.comp: Inertia (\%) explained for all dimensions of compromise matrix.
+#'  @slot  Compromise.Coords: Projection of all observations in compromise (Coords).
+#'  @slot  Compromise.Matrix: Compromise Matrix from statis methodology.
+#'  @slot  RQO: Representation Quality of observations in compromise matrix.
+#'  @slot  TableProjections: Projection of each table on Compromise configuration
+#'
 #' @section DistStatis-general-functions:
 #' \describe{
 #'  \item{DistStatis}{Getters for their respective slots.}
@@ -40,33 +39,268 @@
 #'
 #' @examples
 #' {
-#' showClass("DistStatis")
+#' showClass('DistStatis')
 #' }
-
+#' @docType methods
 #' @name DistStatis-class
 #' @rdname DistStatis-Class
 #' @export
 #'
 #'
 #'
-setClass(Class="DistStatis",
-         slots=c(
-           Inertia.RV="data.frame",
-           RV="matrix",
-           Euclid.Im="data.frame",
-           Inertia.comp="matrix",
-           Compromise.Coords="data.frame",
-           Compromise.Matrix="data.frame",
-           RQO="data.frame",
-           TableProjections="list"),
-         prototype=prototype(
-           Inertia.RV=data.frame(),
-           RV=matrix(),
-           Euclid.Im=data.frame(),
-           Inertia.comp=matrix(),
-           Compromise.Coords=data.frame(),
-           Compromise.Matrix=data.frame(),
-           RQO=data.frame(),
-           TableProjections=list())
-)
+setClass(Class = "DistStatis", slots = c(Inertia.RV = "data.frame", RV = "matrix", Euclid.Im = "data.frame",
+    Inertia.comp = "matrix", Compromise.Coords = "data.frame", Compromise.Matrix = "data.frame", RQO = "data.frame",
+    TableProjections = "list"), prototype = prototype(Inertia.RV = data.frame(), RV = matrix(), Euclid.Im = data.frame(),
+    Inertia.comp = matrix(), Compromise.Coords = data.frame(), Compromise.Matrix = data.frame(), RQO = data.frame(),
+    TableProjections = list()))
+
+
+setGeneric(name = "Inertia_RV", def = function(x) {
+    standardGeneric("Inertia_RV")
+})
+setGeneric(name = "correl", def = function(x) {
+    standardGeneric("correl")
+})
+setGeneric(name = "Euclid_Im", def = function(x) {
+    standardGeneric("Euclid_Im")
+})
+setGeneric(name = "Inertia_comp", def = function(x) {
+    standardGeneric("Inertia_comp")
+})
+setGeneric(name = "compromise_coords", def = function(x) {
+    standardGeneric("compromise_coords")
+})
+setGeneric(name = "Compromise_matrix", def = function(x) {
+    standardGeneric("Compromise_matrix")
+})
+setGeneric(name = "RQO", def = function(x) {
+    standardGeneric("RQO")
+})
+setGeneric(name = "Trajectories", def = function(x) {
+    standardGeneric("Trajectories")
+})
+
+#' @title Inertia_RV
+#' @description Accessor to Inertia_RV from LinkData output.
+#' @param x an object from DistSatis class.
+#' @name Inertia_RV
+#' @aliases  Inertia_RV,DistStatis-method
+#' @docType methods
+#' @rdname Inertia_RV-methods
+#' @export
+#' @return  Inertia_RV explained inertia for RV matrix from LinkData object
+#' @examples
+#' {
+#'data(Taraoceans)
+#'pro.phylo <- Taraoceans$taxonomy[ ,'Phylum']
+#'TaraOc<-list(Taraoceans$phychem,as.data.frame(Taraoceans$pro.phylo)
+#',as.data.frame(Taraoceans$pro.NOGs))
+#'TaraOc_1<-scale(TaraOc[[1]])
+#'Normalization<-lapply(list(TaraOc[[2]],TaraOc[[3]]),
+#'function(x){DataProcessing(x,Method='Compositional')})
+#'colnames(Normalization[[1]])=pro.phylo
+#'colnames(Normalization[[2]])=Taraoceans$GO
+#'TaraOc<-list(TaraOc_1,Normalization[[1]],Normalization[[2]])
+#'names(TaraOc)<-c('phychem','pro_phylo','pro_NOGs')
+#'TaraOc<-lapply(TaraOc,as.data.frame)
+#'Output<-LinkData(TaraOc,Scale =FALSE,Distance = c('ScalarProduct','Euclidean','Euclidean'))
+#'Inertia_RV(Output)
+#' }
+setMethod("Inertia_RV", "DistStatis", function(x) (x@Inertia.RV))
+
+#' @title correl
+#' @description Accessor to RV (Vectorial correlation coefficient) from LinkData output.
+#' @param x an object from DistSatis class.
+#' @name correl
+#' @aliases  correl,DistStatis-method
+#' @docType methods
+#' @rdname correl-methods
+#' @export
+#' @return  RV correlation coefficient for each input table to  LinkData function
+#' @examples
+#' {
+#'data(Taraoceans)
+#'pro.phylo <- Taraoceans$taxonomy[ ,'Phylum']
+#'TaraOc<-list(Taraoceans$phychem,as.data.frame(Taraoceans$pro.phylo)
+#',as.data.frame(Taraoceans$pro.NOGs))
+#'TaraOc_1<-scale(TaraOc[[1]])
+#'Normalization<-lapply(list(TaraOc[[2]],TaraOc[[3]]),
+#'function(x){DataProcessing(x,Method='Compositional')})
+#'colnames(Normalization[[1]])=pro.phylo
+#'colnames(Normalization[[2]])=Taraoceans$GO
+#'TaraOc<-list(TaraOc_1,Normalization[[1]],Normalization[[2]])
+#'names(TaraOc)<-c('phychem','pro_phylo','pro_NOGs')
+#'TaraOc<-lapply(TaraOc,as.data.frame)
+#'Output<-LinkData(TaraOc,Scale =FALSE,Distance = c('ScalarProduct','Euclidean','Euclidean'))
+#'correl(Output)
+#' }
+setMethod("correl", "DistStatis", function(x) (x@RV))
+#' @title Euclid_Im
+#' @description Accessor to the Observations Image Euclidean, i.e. the projections from LinkData output.
+#' @param x an object from DistSatis class.
+#' @name Euclid_Im
+#' @aliases  Euclid_Im,DistStatis-method
+#' @docType methods
+#' @rdname Euclid_Im-methods
+#' @export
+#' @return  Euclid_Im Euclidean image of the input tables in LinData function.
+#' @examples
+#' {
+#'data(Taraoceans)
+#'pro.phylo <- Taraoceans$taxonomy[ ,'Phylum']
+#'TaraOc<-list(Taraoceans$phychem,as.data.frame(Taraoceans$pro.phylo)
+#',as.data.frame(Taraoceans$pro.NOGs))
+#'TaraOc_1<-scale(TaraOc[[1]])
+#'Normalization<-lapply(list(TaraOc[[2]],TaraOc[[3]]),
+#'function(x){DataProcessing(x,Method='Compositional')})
+#'colnames(Normalization[[1]])=pro.phylo
+#'colnames(Normalization[[2]])=Taraoceans$GO
+#'TaraOc<-list(TaraOc_1,Normalization[[1]],Normalization[[2]])
+#'names(TaraOc)<-c('phychem','pro_phylo','pro_NOGs')
+#'TaraOc<-lapply(TaraOc,as.data.frame)
+#'Output<-LinkData(TaraOc,Scale =FALSE,Distance = c('ScalarProduct','Euclidean','Euclidean'))
+#'Euclid_Im(Output)
+#' }
+setMethod("Euclid_Im", "DistStatis", function(x) (x@Euclid.Im))
+
+#' @title Inertia_comp
+#' @description Accessor to explained inertia of compromise axis from LinkData output.
+#' @param x an object from DistSatis class.
+#' @name Inertia_comp
+#' @aliases  Inertia_comp,DistStatis-method
+#' @docType methods
+#' @rdname Inertia_comp-methods
+#' @export
+#' @return  Inertia_comp explained inertia for Compromise matrix from LinkData object
+#' @examples
+#' {
+#'data(Taraoceans)
+#'pro.phylo <- Taraoceans$taxonomy[ ,'Phylum']
+#'TaraOc<-list(Taraoceans$phychem,as.data.frame(Taraoceans$pro.phylo)
+#',as.data.frame(Taraoceans$pro.NOGs))
+#'TaraOc_1<-scale(TaraOc[[1]])
+#'Normalization<-lapply(list(TaraOc[[2]],TaraOc[[3]]),
+#'function(x){DataProcessing(x,Method='Compositional')})
+#'colnames(Normalization[[1]])=pro.phylo
+#'colnames(Normalization[[2]])=Taraoceans$GO
+#'TaraOc<-list(TaraOc_1,Normalization[[1]],Normalization[[2]])
+#'names(TaraOc)<-c('phychem','pro_phylo','pro_NOGs')
+#'TaraOc<-lapply(TaraOc,as.data.frame)
+#'Output<-LinkData(TaraOc,Scale =FALSE,Distance = c('ScalarProduct','Euclidean','Euclidean'))
+#'Inertia_comp(Output)
+#' }
+setMethod("Inertia_comp", "DistStatis", function(x) (x@Inertia.comp))
+
+#' @title compromise_coords
+#' @description Accessor to compromise coordinates from LinkData output.
+#' @param x an object from DistSatis class.
+#' @name compromise_coords
+#' @aliases  compromise_coords,DistStatis-method
+#' @docType methods
+#' @rdname compromise_coords-methods
+#' @export
+#' @return  compromise_coords coordinates of observations in the compromise configuration from LinkData function
+#' @examples
+#' {
+#'data(Taraoceans)
+#'pro.phylo <- Taraoceans$taxonomy[ ,'Phylum']
+#'TaraOc<-list(Taraoceans$phychem,as.data.frame(Taraoceans$pro.phylo)
+#',as.data.frame(Taraoceans$pro.NOGs))
+#'TaraOc_1<-scale(TaraOc[[1]])
+#'Normalization<-lapply(list(TaraOc[[2]],TaraOc[[3]]),
+#'function(x){DataProcessing(x,Method='Compositional')})
+#'colnames(Normalization[[1]])=pro.phylo
+#'colnames(Normalization[[2]])=Taraoceans$GO
+#'TaraOc<-list(TaraOc_1,Normalization[[1]],Normalization[[2]])
+#'names(TaraOc)<-c('phychem','pro_phylo','pro_NOGs')
+#'TaraOc<-lapply(TaraOc,as.data.frame)
+#'Output<-LinkData(TaraOc,Scale =FALSE,Distance = c('ScalarProduct','Euclidean','Euclidean'))
+#'compromise_coords(Output)
+#' }
+setMethod("compromise_coords", "DistStatis", function(x) (x@Compromise.Coords))
+
+#' @title Compromise_matrix
+#' @description Accessor to Compromise Matrix from LinkData output.
+#' @param x an object from DistSatis class.
+#' @name Compromise_matrix
+#' @aliases  Compromise_matrix,DistStatis-method
+#' @docType methods
+#' @rdname Compromise_matrix-methods
+#' @export
+#' @return  Compromise_matrix: Compromise matrix from LinkData object
+#' @examples
+#' {
+#'data(Taraoceans)
+#'pro.phylo <- Taraoceans$taxonomy[ ,'Phylum']
+#'TaraOc<-list(Taraoceans$phychem,as.data.frame(Taraoceans$pro.phylo)
+#',as.data.frame(Taraoceans$pro.NOGs))
+#'TaraOc_1<-scale(TaraOc[[1]])
+#'Normalization<-lapply(list(TaraOc[[2]],TaraOc[[3]]),
+#'function(x){DataProcessing(x,Method='Compositional')})
+#'colnames(Normalization[[1]])=pro.phylo
+#'colnames(Normalization[[2]])=Taraoceans$GO
+#'TaraOc<-list(TaraOc_1,Normalization[[1]],Normalization[[2]])
+#'names(TaraOc)<-c('phychem','pro_phylo','pro_NOGs')
+#'TaraOc<-lapply(TaraOc,as.data.frame)
+#'Output<-LinkData(TaraOc,Scale =FALSE,Distance = c('ScalarProduct','Euclidean','Euclidean'))
+#'Compromise_matrix(Output)
+#' }
+setMethod("Compromise_matrix", "DistStatis", function(x) (x@Compromise.Matrix))
+#' @title RQO
+#' @description Accessor to RQO (% of individuals representation) from LinkData output.
+#' @param x an object from DistSatis class.
+#' @name RQO
+#' @aliases  RQO,DistStatis-method
+#' @docType methods
+#' @rdname RQO-methods
+#' @export
+#' @return  RQO Representation Quality of the observations in the compromise configuration from LinkData object
+#' @examples
+#' {
+#'data(Taraoceans)
+#'pro.phylo <- Taraoceans$taxonomy[ ,'Phylum']
+#'TaraOc<-list(Taraoceans$phychem,as.data.frame(Taraoceans$pro.phylo)
+#',as.data.frame(Taraoceans$pro.NOGs))
+#'TaraOc_1<-scale(TaraOc[[1]])
+#'Normalization<-lapply(list(TaraOc[[2]],TaraOc[[3]]),
+#'function(x){DataProcessing(x,Method='Compositional')})
+#'colnames(Normalization[[1]])=pro.phylo
+#'colnames(Normalization[[2]])=Taraoceans$GO
+#'TaraOc<-list(TaraOc_1,Normalization[[1]],Normalization[[2]])
+#'names(TaraOc)<-c('phychem','pro_phylo','pro_NOGs')
+#'TaraOc<-lapply(TaraOc,as.data.frame)
+#'Output<-LinkData(TaraOc,Scale =FALSE,Distance = c('ScalarProduct','Euclidean','Euclidean'))
+#'RQO(Output)
+#' }
+setMethod("RQO", "DistStatis", function(x) (x@RQO))
+
+#' @title Trajectories
+#' @description Accessor to projections into the common configuration, i.e. compromise of each input table from LinkData output.
+#' @param x an object from DistSatis class.
+#' @name Trajectories
+#' @aliases  Trajectories,DistStatis-method
+#' @docType methods
+#' @rdname Trajectories-methods
+#' @export
+#' @return  Trajectories contains a list of the projections
+#' of each input table into the common configuration, i.e. the compromise
+#' from LinkData object
+#' @examples
+#' {
+#'data(Taraoceans)
+#'pro.phylo <- Taraoceans$taxonomy[ ,'Phylum']
+#'TaraOc<-list(Taraoceans$phychem,as.data.frame(Taraoceans$pro.phylo)
+#',as.data.frame(Taraoceans$pro.NOGs))
+#'TaraOc_1<-scale(TaraOc[[1]])
+#'Normalization<-lapply(list(TaraOc[[2]],TaraOc[[3]]),
+#'function(x){DataProcessing(x,Method='Compositional')})
+#'colnames(Normalization[[1]])=pro.phylo
+#'colnames(Normalization[[2]])=Taraoceans$GO
+#'TaraOc<-list(TaraOc_1,Normalization[[1]],Normalization[[2]])
+#'names(TaraOc)<-c('phychem','pro_phylo','pro_NOGs')
+#'TaraOc<-lapply(TaraOc,as.data.frame)
+#'Output<-LinkData(TaraOc,Scale =FALSE,Distance = c('ScalarProduct','Euclidean','Euclidean'))
+#'Trajectories(Output)
+#' }
+setMethod("Trajectories", "DistStatis", function(x) (x@TableProjections))
 
